a0b9cd4126923fed0b30d1713e8d30c7ff613678
Taras Mykhalevych <taras.mykhalevych@gmail.com>
2023-08-16 19:29:56 +0000

    style: Simplify structure by grouping all traces together

--- src/freertos/traces.cpp
+++ src/freertos/traces.cpp
@@ -8,63 +8,107 @@
 
 #include <string_view>
 
-extern "C" void vTracesInit(uint8_t* puBuff, size_t uxCapasity, uxGetTime* pfnGetSteadyTimestamp,
-                            xPrintTraces* pfnOutputMethod)
+using namespace event_tracer::freertos;
+
+#ifdef __cplusplus
+extern "C"
 {
-    using namespace event_tracer::freertos;
+#endif
 
-    struct EventTracerContext
+    void vTraceTaskCreate(void* xTask)
     {
-        xPrintTraces* data_cb;
-    };
+        tracer().register_event(Event::TASK_CREATE, static_cast<TaskHandle_t>(xTask));
+    }
 
-    struct DataReadyMessage
+    void vTraceTaskDelete(void* xTask)
     {
-        EventRegistry* registry = nullptr;
-        EventTracer::data_done_cb_t done_cb;
-    };
+        tracer().register_event(Event::TASK_DELETE, static_cast<TaskHandle_t>(xTask));
+    }
 
-    /// the size for the data traces queue, 2 is minimal value, that should be enough
-    static constexpr auto DATA_REGISTRY_QUEUE_SIZE = 2;
-    // the queue is instanciated after event tracer to prevent traces when tracer is not ready
-    static QueueHandle_t data_ready_queue = nullptr;
+    void vTraceTaskSwitchedIn(void* pxCurrentTCB)
+    {
+        const auto timestamp = tracer().now();
+        static void* pxPreviousTCB = nullptr;
+        if (pxPreviousTCB != pxCurrentTCB) {
+            tracer().register_event(Event::TASK_SWITCHED_IN, static_cast<TaskHandle_t>(pxCurrentTCB), timestamp);
+            pxPreviousTCB = pxCurrentTCB;
+        }
+    }
 
-    const auto tracer_task = [](void* context) {
-        ET_ASSERT(context);
-        ET_ASSERT(data_ready_queue);
+    void vTraceSystemTick(size_t uiTickCount)
+    {
+        // TODO: implement
+    }
 
-        auto& callbacks = *reinterpret_cast<EventTracerContext*>(context);
-        DataReadyMessage msg;
+    void vTraceMalloc([[maybe_unused]] void* pvAddress, [[maybe_unused]] size_t uiSize)
+    {
+        tracer().register_event(Event::MALLOC);
+    }
 
-        while (true) {
-            if (xQueueReceive(data_ready_queue, &msg, portMAX_DELAY)) {
-                ET_ASSERT(msg.registry);
-                for (const auto& event : *msg.registry) {
-                    callbacks.data_cb(format(event).data());
+    void vTraceFree([[maybe_unused]] void* pvAddress, [[maybe_unused]] size_t uiSize)
+    {
+        tracer().register_event(Event::FREE);
+    }
+
+    void vTracesInit(uint8_t* puBuff, size_t uxCapasity, uxGetTime* pfnGetSteadyTimestamp,
+                     xPrintTraces* pfnOutputMethod)
+    {
+        struct EventTracerContext
+        {
+            xPrintTraces* data_cb;
+        };
+
+        struct DataReadyMessage
+        {
+            EventRegistry* registry = nullptr;
+            EventTracer::data_done_cb_t done_cb;
+        };
+
+        /// the size for the data traces queue, 2 is minimal value, that should be enough
+        static constexpr auto DATA_REGISTRY_QUEUE_SIZE = 2;
+        // the queue is instanciated after event tracer to prevent traces when tracer is not ready
+        static QueueHandle_t data_ready_queue = nullptr;
+
+        const auto tracer_task = [](void* context) {
+            ET_ASSERT(context);
+            ET_ASSERT(data_ready_queue);
+
+            auto& callbacks = *reinterpret_cast<EventTracerContext*>(context);
+            DataReadyMessage msg;
+
+            while (true) {
+                if (xQueueReceive(data_ready_queue, &msg, portMAX_DELAY)) {
+                    ET_ASSERT(msg.registry);
+                    for (const auto& event : *msg.registry) {
+                        callbacks.data_cb(format(event).data());
+                    }
+                    // notify event tracer that we're done handling the data
+                    msg.done_cb();
                 }
-                // notify event tracer that we're done handling the data
-                msg.done_cb();
             }
-        }
-    };
+        };
 
-    const auto data_ready_handler = [](EventRegistry& registry, EventTracer::data_done_cb_t done_cb) {
-        ET_ASSERT(data_ready_queue);
+        const auto data_ready_handler = [](EventRegistry& registry, EventTracer::data_done_cb_t done_cb) {
+            ET_ASSERT(data_ready_queue);
 
-        DataReadyMessage msg{.registry = &registry, .done_cb = done_cb};
+            DataReadyMessage msg{.registry = &registry, .done_cb = done_cb};
 
-        if (xQueueSend(data_ready_queue, &msg, 0 /* don't wait */) != pdPASS) {
-            ET_ERROR("Failed to send tracing data");
-        }
-    };
+            if (xQueueSend(data_ready_queue, &msg, 0 /* don't wait */) != pdPASS) {
+                ET_ERROR("Failed to send tracing data");
+            }
+        };
 
-    static EventTracer tracer(reinterpret_cast<std::byte*>(puBuff), uxCapasity, data_ready_handler);
-    EventTracer::set_single_instance(&tracer);
+        static EventTracer tracer(reinterpret_cast<std::byte*>(puBuff), uxCapasity, data_ready_handler);
+        EventTracer::set_single_instance(&tracer);
 
-    tracer.set_time_getter(pfnGetSteadyTimestamp);
-    data_ready_queue = xQueueCreate(DATA_REGISTRY_QUEUE_SIZE, sizeof(DataReadyMessage));
+        tracer.set_time_getter(pfnGetSteadyTimestamp);
+        data_ready_queue = xQueueCreate(DATA_REGISTRY_QUEUE_SIZE, sizeof(DataReadyMessage));
 
-    static EventTracerContext context{.data_cb = pfnOutputMethod};
-    xTaskCreate(tracer_task, "event_tracer", configMINIMAL_STACK_SIZE, &context, (configTIMER_TASK_PRIORITY - 1),
-                nullptr);
-}
+        static EventTracerContext context{.data_cb = pfnOutputMethod};
+        xTaskCreate(tracer_task, "event_tracer", configMINIMAL_STACK_SIZE, &context, (configTIMER_TASK_PRIORITY - 1),
+                    nullptr);
+    }
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
--- src/freertos/traces.h
+++ src/freertos/traces.h
@@ -6,41 +6,20 @@
 #define configUSE_TRACE_FACILITY 1
 #define INCLUDE_xTaskGetCurrentTaskHandle 1
 
-#ifdef tracerUSE_TASK_TRACES
-#include "task_traces.h"
-#endif
-#ifdef tracerUSE_TASK_PERF
-#include "task_perf.h"
-#endif
-#ifdef tracerUSE_QUEUE_TRACES
-#error Queue traces are not implemented yet
-#endif
-#ifdef tracerUSE_SYNCH_TRACES
-#error Sync traces are not implemented yet
-#endif
-#ifdef tracerUSE_SBUFF_TRACES
-#error Stream buffer traces are not implemented yet
-#endif
-#ifdef tracerUSE_TIMER_TRACES
-#error Timer traces are not implemented yet
-#endif
-#ifdef tracerUSE_POWER_TRACES
-#error Power traces are not implemented yet
-#endif
-#ifdef tracerUSE_ALLOC_TRACES
-#include "alloc_traces.h"
-#endif
-#ifdef tracerUSE_EGROUP_TRACES
-#error Event group traces are not implemented yet
-#endif
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+    void vTraceTaskCreate(void *xTask);
+    void vTraceTaskDelete(void *xTask);
+    void vTraceTaskSwitchedIn(void *pxCurrentTCB);
+    void vTraceSystemTick(size_t uiTickCount);
+    void vTraceMalloc(void *pvAddress, size_t uiSize);
+    void vTraceFree(void *pvAddress, size_t uiSize);
+
     /// @brief Alias for user-defined print function, to provide them with tracing results
-    typedef int xPrintTraces(const char* pszFormat, ...);
+    typedef int xPrintTraces(const char *pszFormat, ...);
 
     /// @brief Alias for user-defined time getter
     typedef uint64_t uxGetTime(void);
@@ -50,9 +29,66 @@ extern "C"
     /// @param uxCapasity Capacity of the buffer made for storing actual tracing data
     /// @param pfnGetSteadyTimestamp Pointer to function that allows to get steady timestamps
     /// @param pfnOutputMethod User defined provider for tracing data
-    void vTracesInit(uint8_t* puBuff, size_t uxCapasity, uxGetTime* pfnGetSteadyTimestamp,
-                     xPrintTraces* pfnOutputMethod);
+    void vTracesInit(uint8_t *puBuff, size_t uxCapasity, uxGetTime *pfnGetSteadyTimestamp,
+                     xPrintTraces *pfnOutputMethod);
 
 #ifdef __cplusplus
 }  // extern "C"
+#endif
+
+#ifdef tracerUSE_TASK_TRACES
+// #define traceMOVED_TASK_TO_READY_STATE(xTask)
+// #define tracePOST_MOVED_TASK_TO_READY_STATE(pxTCB)
+#define traceTASK_CREATE(xTask) vTraceTaskCreate(xTask)
+// #define traceTASK_CREATE_FAILED(pxNewTCB)
+// #define traceTASK_DELAY()
+// #define traceTASK_DELAY_UNTIL()
+#define traceTASK_DELETE(xTask) vTraceTaskDelete(xTask)
+// #define traceTASK_NOTIFY(uxIndexToNotify)
+// #define traceTASK_NOTIFY_FROM_ISR(uxIndexToNotify)
+// #define traceTASK_NOTIFY_GIVE_FROM_ISR(uxIndexToNotify)
+// #define traceTASK_NOTIFY_TAKE(uxIndexToWait)
+// #define traceTASK_NOTIFY_TAKE_BLOCK(uxIndexToWait)
+// #define traceTASK_NOTIFY_WAIT(uxIndexToWait)
+// #define traceTASK_NOTIFY_WAIT_BLOCK(uxIndexToWait)
+// #define traceTASK_PRIORITY_DISINHERIT(pxTCBOfMutexHolder, uxOriginalPriority)
+// #define traceTASK_PRIORITY_INHERIT(pxTCBOfMutexHolder, uxInheritedPriority)
+// #define traceTASK_PRIORITY_SET(xTask, uxNewPriority)
+// #define traceTASK_RESUME(xTask)
+// #define traceTASK_RESUME_FROM_ISR(xTask)
+// #define traceTASK_SUSPEND(xTask)
+#define traceTASK_SWITCHED_IN() vTraceTaskSwitchedIn(pxCurrentTCB)
+// #define traceTASK_SWITCHED_OUT()
+#endif
+
+#ifdef tracerUSE_TASK_PERF
+#define traceTASK_INCREMENT_TICK(xTickCount) vTraceSystemTick((size_t)xTickCount)
+#endif
+
+#ifdef tracerUSE_QUEUE_TRACES
+#error Queue traces are not implemented yet
+#endif
+
+#ifdef tracerUSE_SYNCH_TRACES
+#error Sync traces are not implemented yet
+#endif
+
+#ifdef tracerUSE_SBUFF_TRACES
+#error Stream buffer traces are not implemented yet
+#endif
+
+#ifdef tracerUSE_TIMER_TRACES
+#error Timer traces are not implemented yet
+#endif
+#ifdef tracerUSE_POWER_TRACES
+#error Power traces are not implemented yet
+#endif
+
+#ifdef tracerUSE_ALLOC_TRACES
+#define traceMALLOC(pvAddress, uiSize) vTraceMalloc(pvAddress, uiSize)
+#define traceFREE(pvAddress, uiSize) vTraceFree(pvAddress, uiSize)
+#endif
+
+#ifdef tracerUSE_EGROUP_TRACES
+#error Event group traces are not implemented yet
 #endif
