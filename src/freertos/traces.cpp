8b4acea557fa956ab4d01cc7a6f6b6f1a7ee758d
Taras Mykhalevych <taras.mykhalevych@gmail.com>
2023-11-23 20:30:56 +0000

    feat: Remove unnecessary exetern "C"

--- src/freertos/traces.cpp
+++ src/freertos/traces.cpp
@@ -10,106 +10,97 @@
 
 using namespace event_tracer::freertos;
 
-#ifdef __cplusplus
-extern "C"
+extern "C" void trace_task_create(void* task)
 {
-#endif
+    tracer().register_event(EventId::TASK_CREATE, static_cast<TaskHandle_t>(task));
+}
 
-    void trace_task_create(void* task)
-    {
-        tracer().register_event(EventId::TASK_CREATE, static_cast<TaskHandle_t>(task));
+extern "C" void trace_task_delete(void* task)
+{
+    tracer().register_event(EventId::TASK_DELETE, static_cast<TaskHandle_t>(task));
+}
+
+extern "C" void trace_task_switched_in(void* current_tcb)
+{
+    const auto timestamp = tracer().now();
+    static void* previous_tcb = nullptr;
+    if (previous_tcb != current_tcb) {
+        tracer().register_event(EventId::TASK_SWITCHED_IN, static_cast<TaskHandle_t>(current_tcb), timestamp);
+        previous_tcb = current_tcb;
     }
+}
 
-    void trace_task_delete(void* task)
+extern "C" void trace_system_tick(size_t tick_count)
+{
+    // TODO: process profiling logic here
+}
+
+extern "C" void trace_malloc([[maybe_unused]] void* addr, [[maybe_unused]] size_t size)
+{
+    tracer().register_event(EventId::MALLOC);
+}
+
+extern "C" void trace_free([[maybe_unused]] void* addr, [[maybe_unused]] size_t size)
+{
+    tracer().register_event(EventId::FREE);
+}
+
+extern "C" void traces_init(TracesSettings settings)
+{
+    struct EventTracerContext
     {
-        tracer().register_event(EventId::TASK_DELETE, static_cast<TaskHandle_t>(task));
-    }
+        print_traces_cb_t* data_cb;
+        size_t polling_interval_ms;
+    };
 
-    void trace_task_switched_in(void* current_tcb)
+    struct DataReadyMessage
     {
-        const auto timestamp = tracer().now();
-        static void* previous_tcb = nullptr;
-        if (previous_tcb != current_tcb) {
-            tracer().register_event(EventId::TASK_SWITCHED_IN, static_cast<TaskHandle_t>(current_tcb), timestamp);
-            previous_tcb = current_tcb;
-        }
-    }
+        EventRegistry* registry = nullptr;
+        EventTracer::data_done_cb_t done_cb;
+    };
 
-    void trace_system_tick(size_t tick_count)
-    {
-        // TODO: implement
-    }
+    // the queue is instanciated after event tracer to prevent traces when tracer is not ready
+    static QueueHandle_t data_ready_queue = nullptr;
 
-    void trace_malloc([[maybe_unused]] void* addr, [[maybe_unused]] size_t size)
-    {
-        tracer().register_event(EventId::MALLOC);
-    }
-
-    void trace_free([[maybe_unused]] void* addr, [[maybe_unused]] size_t size)
-    {
-        tracer().register_event(EventId::FREE);
-    }
-
-    void traces_init(TracesSettings settings)
-    {
-        struct EventTracerContext
-        {
-            print_traces_cb_t* data_cb;
-            size_t polling_interval_ms;
-        };
-
-        struct DataReadyMessage
-        {
-            EventRegistry* registry = nullptr;
-            EventTracer::data_done_cb_t done_cb;
-        };
-
-        // the queue is instanciated after event tracer to prevent traces when tracer is not ready
-        static QueueHandle_t data_ready_queue = nullptr;
-
-        const auto tracer_task = [](void* context) {
-            ET_ASSERT(context);
-            ET_ASSERT(data_ready_queue);
-
-            auto& ctx = *reinterpret_cast<EventTracerContext*>(context);
-            DataReadyMessage msg;
-
-            while (true) {
-                while (xQueueReceive(data_ready_queue, &msg, 0 /* don't block */)) {
-                    ET_ASSERT(msg.registry);
-                    for (const auto& event : *msg.registry) {
-                        ctx.data_cb(format(event).data());
-                    }
-                    // notify event tracer that we're done handling the data
-                    msg.done_cb();
-                }
-                vTaskDelay(ctx.polling_interval_ms / portTICK_PERIOD_MS);
-            }
-        };
-
-        const auto data_ready_handler = [](EventRegistry& registry, EventTracer::data_done_cb_t done_cb) {
-            ET_ASSERT(data_ready_queue);
-
-            DataReadyMessage msg{.registry = &registry, .done_cb = std::move(done_cb)};
-
-            if (xQueueSend(data_ready_queue, &msg, 0 /* don't block */) != pdPASS) {
-                ET_ERROR("Failed to send tracing data");
-            }
-        };
-
-        static EventTracerContext context{.data_cb = settings.print_traces_cb,
-                                          .polling_interval_ms = settings.polling_interval_ms};
-
-        SingleEventTracer::emplace(reinterpret_cast<std::byte*>(settings.buff), settings.capacity, data_ready_handler,
-                                   settings.get_timestamp_cb);
-
-        data_ready_queue = xQueueCreate(settings.data_queue_size, sizeof(DataReadyMessage));
+    const auto tracer_task = [](void* context) {
+        ET_ASSERT(context);
         ET_ASSERT(data_ready_queue);
 
-        xTaskCreate(tracer_task, tracerTASK_NAME, configMINIMAL_STACK_SIZE, &context, (configTIMER_TASK_PRIORITY - 1),
-                    nullptr);
-    }
+        auto& ctx = *reinterpret_cast<EventTracerContext*>(context);
+        DataReadyMessage msg;
 
-#ifdef __cplusplus
-}  // extern "C"
-#endif
+        while (true) {
+            while (xQueueReceive(data_ready_queue, &msg, 0 /* don't block */)) {
+                ET_ASSERT(msg.registry);
+                for (const auto& event : *msg.registry) {
+                    ctx.data_cb(format(event).data());
+                }
+                // notify event tracer that we're done handling the data
+                msg.done_cb();
+            }
+            vTaskDelay(ctx.polling_interval_ms / portTICK_PERIOD_MS);
+        }
+    };
+
+    const auto data_ready_handler = [](EventRegistry& registry, EventTracer::data_done_cb_t done_cb) {
+        ET_ASSERT(data_ready_queue);
+
+        DataReadyMessage msg{.registry = &registry, .done_cb = std::move(done_cb)};
+
+        if (xQueueSend(data_ready_queue, &msg, 0 /* don't block */) != pdPASS) {
+            ET_ERROR("Failed to send tracing data");
+        }
+    };
+
+    static EventTracerContext context{.data_cb = settings.print_traces_cb,
+                                      .polling_interval_ms = settings.polling_interval_ms};
+
+    SingleEventTracer::emplace(reinterpret_cast<std::byte*>(settings.buff), settings.capacity, data_ready_handler,
+                               settings.get_timestamp_cb);
+
+    data_ready_queue = xQueueCreate(settings.data_queue_size, sizeof(DataReadyMessage));
+    ET_ASSERT(data_ready_queue);
+
+    xTaskCreate(tracer_task, tracerTASK_NAME, configMINIMAL_STACK_SIZE, &context, (configTIMER_TASK_PRIORITY - 1),
+                nullptr);
+}
--- src/freertos/traces.h
+++ src/freertos/traces.h
@@ -25,48 +25,39 @@ static_assert(tracerMAX_EVENT_MESSAGE_LEN >= configMAX_TASK_NAME_LEN);
 #define tracerTASK_NAME "event_tracer"
 #endif
 
-#ifdef __cplusplus
-extern "C"
+void trace_task_create(void *task);
+void trace_task_delete(void *task);
+void trace_task_switched_in(void *current_tcb);
+void trace_system_tick(size_t tick_count);
+void trace_malloc(void *addr, size_t size);
+void trace_free(void *addr, size_t size);
+
+/// @brief Alias for user-defined print function, to provide them with tracing results
+typedef int print_traces_cb_t(const char *format, ...);
+
+/// @brief Alias for user-defined time getter
+typedef uint64_t get_timestamp_cb_t(void);
+
+/// @brief Traces settings
+typedef struct
 {
-#endif
+    /// Pointer to buffer made for storing actual tracing data
+    uint8_t *buff;
+    /// Capacity of the buffer made for storing actual tracing data
+    size_t capacity;
+    /// Pointer to function that allows to get steady timestamps
+    get_timestamp_cb_t *get_timestamp_cb;
+    /// User defined provider for tracing data
+    print_traces_cb_t *print_traces_cb;
+    /// The size for the data traces queue, 2 is minimal value, that should be enough
+    int data_queue_size;
+    /// Data traces queue polling interval
+    size_t polling_interval_ms;
+} TracesSettings;
 
-    void trace_task_create(void *task);
-    void trace_task_delete(void *task);
-    void trace_task_switched_in(void *current_tcb);
-    void trace_system_tick(size_t tick_count);
-    void trace_malloc(void *addr, size_t size);
-    void trace_free(void *addr, size_t size);
-
-    /// @brief Alias for user-defined print function, to provide them with tracing results
-    typedef int print_traces_cb_t(const char *format, ...);
-
-    /// @brief Alias for user-defined time getter
-    typedef uint64_t get_timestamp_cb_t(void);
-
-    /// @brief Traces settings
-    typedef struct
-    {
-        /// Pointer to buffer made for storing actual tracing data
-        uint8_t *buff;
-        /// Capacity of the buffer made for storing actual tracing data
-        size_t capacity;
-        /// Pointer to function that allows to get steady timestamps
-        get_timestamp_cb_t *get_timestamp_cb;
-        /// User defined provider for tracing data
-        print_traces_cb_t *print_traces_cb;
-        /// The size for the data traces queue, 2 is minimal value, that should be enough
-        int data_queue_size;
-        /// Data traces queue polling interval
-        size_t polling_interval_ms;
-    } TracesSettings;
-
-    /// @brief Initiates FreeRTOS event-tracer
-    /// @param settings Traces settings
-    void traces_init(TracesSettings settings);
-
-#ifdef __cplusplus
-}  // extern "C"
-#endif
+/// @brief Initiates FreeRTOS event-tracer
+/// @param settings Traces settings
+void traces_init(TracesSettings settings);
 
 #ifdef tracerUSE_TASK_TRACES
 // #define traceMOVED_TASK_TO_READY_STATE(task)
