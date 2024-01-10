5b3a2717de903dced7d3791ca303f8bdb0a80ae2
Taras Mykhalevych <taras.mykhalevych@gmail.com>
2024-01-10 16:57:26 +0000

    fix: Fix assert

--- src/core/assert.hpp
+++ src/core/assert.hpp
@@ -3,7 +3,7 @@
 #ifdef tracerUSE_ASSERT_HOOK
 void event_tracer_assert(const char* const file, unsigned long line);
 #define ET_ASSERT(expr) \
-    if (expr) event_tracer_assert(__FILE__, __LINE__)
+    if (!(expr)) event_tracer_assert(__FILE__, __LINE__)
 #else
 #include <cassert>
 #define ET_ASSERT(expr) assert(expr)
