8f1ef64900736244878a632cdc473be369fd886a
Taras Mykhalevych <taras.mykhalevych@gmail.com>
2024-01-21 13:44:37 +0000

    feat: Add basic memcheck test

---
+++ scripts/memcheck.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+current_dir="$(pwd)"
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+build_dir="$script_dir/../build"
+out_dir="$build_dir/memcheck"
+
+target="$current_dir/$1"
+test_duration_sec=10
+
+mkdir -p "$out_dir"
+cd $out_dir
+
+echo "Start testing $target for $test_duration_sec sec"
+
+report="$out_dir/memckeck.txt"
+
+valgrind --leak-check=full "$target" > "$report" 2>&1 &
+target_pid=$!
+sleep $test_duration_sec
+kill -SIGINT $target_pid
+
+echo "Stop testing $target"
+echo "Report ready: $report"
+
+cd $current_dir
